72d016dbfa9705d5049fb1e5ed1bedfdea462bac
LucasHubnerDev <hubner.dev@gmail.com>
2026-03-20 03:48:53 -0400

    operadores aritiméticos

---
+++ javascript/operadores-matematicos.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 5;
+
+console.log(a + b); // 15 (adição)
+console.log(a - b); // 5 (subtração)
+console.log(a * b); // 50 (multiplicação)
+console.log(a / b); // 2 (divisão)
+console.log(a % b); // 0 (resto da divisão)
+console.log(a ** b); // 100000 (potência)
